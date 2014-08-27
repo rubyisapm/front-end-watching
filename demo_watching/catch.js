95e5774acc94749a0169d56b7994b8d9eed00897
skysunshine99 <skysunshine99@126.com>
2014-08-27 22:07:06 +0800

    base

--- demo_watching/catch.js
+++ demo_watching/catch.js
@@ -5,19 +5,49 @@ var http=require("http");
 var eventEmitter=require('events').EventEmitter;
 var queryString=require("querystring");
 var server=http.createServer(function(req,res){
-    var postData="";
-    req.setEncoding='uff-8';
-    res.writeHead(200,{
-        "Content-type":"text/html;charset=utf-8"
-    })
-    req.on('data',function(data){
-        postData+=data;
-    })
-    res.write('<form action="locahost:8001/" method="post"><input type="text"/><input type="submit" value="submit"/></form>');
-    req.on("end",function(){
-        console.log(postData);
-        res.end(postData);
-    })
+    if("/"==req.url){
+        res.writeHead(200,{
+            "Content-type":"text-html;charset=utf-8"
+        });
+        res.end([
+            '<form method="POST" action="/submitInfo">'
+            ,'<label for="dotId">dotId: </label>'
+            ,'<input type="text" name="dotId"/>'
+            ,'<label for="dotDesc">dotDesc: </label>'
+            ,'<input type="text" name="dotDesc"/>'
+            ,'<label for="time">time: </label>'
+            ,'<input type="text" name="time"/>'
+            ,'<input type="submit" value="submit" id="submit"/>'
+            ,'</form>'
+            ,'<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>'
+            ,'<script type="text/javascript">'
+            ,'$("#submit").click(function(){' +
+                '$.ajax({' +
+                '       type:"POST",' +
+                '       url:/submitInfo,' +
+                '       async:false,' +
+                '       data:{dotId:$("input[name=\'dotId\']").val(),dotDesc:$("input[name=\'dotDesc\']").val(),time:$("input[name=\'time\']").val()},' +
+                '       dataType:"json",'+
+                '       success:function(data){' +
+                '           console.log(data);' +
+                '       }' +
+                '})' +
+            '})'
+        ].join(''));
+    }else if("/submitInfo"==req.url && 'POST'==req.method){
+        var postData='';
+        req.on('data',function(data){
+            postData=data;
+        });
+        req.on('end',function(){
+            res.writeHead(200,{
+                "Content-type":"text-html;charset=utf-8"
+            });
+            res.write('You submitted this: '+postData);
+            res.end();
+        });
+
+    }
 }).listen(8001);
 /*
 server.on("connection",function(socket){
