e406a62409ca512b777ac5f4a14b3255ab4b11dc
skysunshine99 <skysunshine99@126.com>
2014-08-30 18:28:41 +0800

    more or less

--- demo_watching/catch.js
+++ demo_watching/catch.js
@@ -2,8 +2,10 @@
  * Created by ruby on 2014/7/31.
  */
 var http=require("http");
-var eventEmitter=require('events').EventEmitter;
 var queryString=require("querystring");
+var mongodb=require("mongodb");
+var mongoClient=mongodb.MongoClient;
+var Db=mongodb.Db;
 var server=http.createServer(function(req,res){
     if("/"==req.url){
         res.writeHead(200,{
@@ -20,22 +22,10 @@ var server=http.createServer(function(req,res){
             ,'<input type="submit" value="submit" id="submit"/>'
             ,'</form>'
             ,'<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>'
-            ,'<script type="text/javascript">'
-            ,'$("#submit").click(function(){' +
-                '$.ajax({' +
-                '       type:"POST",' +
-                '       url:/submitInfo,' +
-                '       async:false,' +
-                '       data:{dotId:$("input[name=\'dotId\']").val(),dotDesc:$("input[name=\'dotDesc\']").val(),time:$("input[name=\'time\']").val()},' +
-                '       dataType:"json",'+
-                '       success:function(data){' +
-                '           console.log(data);' +
-                '       }' +
-                '})' +
-            '})'
         ].join(''));
     }else if("/submitInfo"==req.url && 'POST'==req.method){
         var postData='';
+        req.setEncoding("utf-8");
         req.on('data',function(data){
             postData=data;
         });
@@ -43,18 +33,46 @@ var server=http.createServer(function(req,res){
             res.writeHead(200,{
                 "Content-type":"text-html;charset=utf-8"
             });
-            res.write('You submitted this: '+postData);
+            res.write('You submitted this: '+postData+", the data you send will be sent to mongoDB");
+
+            mongoClient.connect("mongodb://localhost:27017/dots",function(err,db){
+                if(err){
+                   console.log(err);
+                   var db=new Db("dots","localhost:27017");
+                    db.open(function(err,db){
+                      db.createCollection("dotInfo",function(err,collection){
+                          collection.insert(postData,function(err,doc){
+                              collection.find().toArray(function(err,result){
+                                  console.log(result);
+                              })
+                          })
+                      })
+                    })
+                }else{
+                    db.createCollection("dotInfo",function(err,collection){
+                        if(!err){
+                            /*change the time to the current time*/
+                            var dataToInsert=queryString.parse(postData);
+                            dataToInsert.time=new Date();
+                            collection.insert(dataToInsert,function(err,doc){
+                                if(err){
+                                    console.log(err);
+                                }else{
+                                    collection.find().toArray(function(err,result){
+                                        console.log("This is the data you inserted to the mongodb: "+result);
+                                    })
+                                }
+
+                            })
+                        }
+                    })
+                }
+            })
             res.end();
         });
 
     }
 }).listen(8001);
-/*
-server.on("connection",function(socket){
-    console.log("hello");
-})
-server.listen(8001);
-*/
 
 
 
--- demo_watching/saveToMongo.js
+++ demo_watching/saveToMongo.js
@@ -1,6 +1,7 @@
-/**
- * Created by ruby on 2014/8/5.
- */
+/*
+ * Created by ruby on 2014/8/5
+*/
+//just test
 // Retrieve
 
 var mongodb=require("mongodb");
@@ -9,7 +10,7 @@ var MongoClient = mongodb.MongoClient;
 var Server=mongodb.Server;
 var format= require('util').format;
 var db=new Db("dot",new Server("localhost",27017));
-db.open(function(err,db){
+/*db.open(function(err,db){
     db.createCollection("test",function(err,collection){
         collection.insert({name:"ruby",time:new Date().toDateString()},function(err,doc){
             collection.find().toArray(function(err,result){
@@ -17,12 +18,22 @@ db.open(function(err,db){
             })
         })
     })
-});
-/*MongoClient.connect("mongodb://localhost:27017/dots",function(err,db){
+});*/
+MongoClient.connect("mongodb://localhost:27017/dots",function(err,db){
+    /*db.dropDatabase(function(err,result){
+        console.log(result);
+    })*/
     db.collection("test",function(err,collection){
-        collection.update({name:"ruby"},{$set:{time:"changed"}},function(err,result){
-            console.log(result);
+        collection.insert({},function(err,doc){
+            collection.find().toArray(function(err,result){
+                console.log(result);
+            })
         })
+     /*  collection.update({name:"ruby"},{$set:{time:"changed"}},function(err,result){
+            console.log(result);
+        })*/
     })
-})*/
+})
+
+
 
